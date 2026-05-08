6fd74c30b3449f11aab70520e300a93d9fd9b7c6
prajwalshettar43 <prajwalshettar43@gmail.com>
2026-05-08 20:28:38 +0530

    Cache on

--- src/store/mapSlice.js
+++ src/store/mapSlice.js
@@ -25,9 +25,9 @@ export const fetchAllEmpirePolygons = createAsyncThunk(
       const state = getState();
       const year = state.map.year;
       // Fetch exactly what is needed for this year
-      //Change boolean to false to turn off the caching and true to turn on the caching
+      //Force Fetch the Data ->  Change boolean to false to turn on the caching and true to turn off the caching
       if(year>2000)return [];
-      const yearData = await loadEmpiresByYearCached(year, true, dispatch);
+      const yearData = await loadEmpiresByYearCached(year, false, dispatch);
       return yearData;
     } catch (err) {
       dispatch(setLoading(false));
